648f5c4f8994ba026dd9dfa13022c0121e5af5fc
TnovyLoo <loobrome@gmail.com>
2022-08-20 14:38:18 +0200

    Creating Profile django model.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
+
+User = get_user_model()
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    # bio = models.TextField(blank=True)
+
+    #Profile images
+    profile_img = models.ImageField(upload_to='profile_images',
+                                    default='pexels-charles-1851164.jpg')
+    wallpaper_img = models.ImageField(upload_to='wallpaper_images',
+                                      default='pexels-amber-janssens-7327624.jpg')
+
+    #Places where user live.
+    location = models.CharField(max_length=100, blank=True)
+    current_location = models.CharField(max_length=100, blank=True)
+
+    #Education
+    school = models.CharField(max_length=100, blank=True)
+
+    #Contact info
+    number = models.IntegerField(max_length=9, blank=True)
+    email = models.EmailField(max_length=254, blank=True)
+
+    #Basic info
+    gender = models.CharField(max_length=100, blank=True)
+    birthday = models.CharField(max_length=50, blank=True)
+    birthyear = models.CharField(max_length=4, blank=True)
+
+    def __str__(self):
+        return self.user.username
