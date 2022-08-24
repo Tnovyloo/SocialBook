bad318a77b18d182458f497eef5d32ab603a4d8f
Szymon Gryz <szymongryz@interia.pl>
2022-08-24 10:58:57 +0200

    settings page

---
+++ static/settings-page/settings.js
@@ -0,0 +1,11 @@
+let basicInfo = document.getElementById('basic-info')
+let places = document.getElementById('live-places')
+let hobbyEducation = document.getElementById('hobby-education')
+let contact = document.getElementById('contact')
+let closePeople = document.getElementById('close-people')
+
+basicInfo.style.display = 'block'
+places.style.display = 'none'
+hobbyEducation.style.display = 'none'
+contact.style.display = 'none'
+closePeople.style.display = 'none'
