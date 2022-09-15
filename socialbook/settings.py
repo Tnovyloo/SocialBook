3731ab66dbbd4be55f8b60c99e430672c926f366
Szymon Gryz <szymongryz@interia.pl>
2022-09-15 20:51:10 +0200

    darkmode for settings page repaired

--- socialbook/settings.py
+++ socialbook/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'socialbook',
         'USER': 'root',
-        'PASSWORD': '***REMOVED***',
+        'PASSWORD': 'szymon1',
         'HOST': 'localhost',
         'PORT': '3306'
     }
